b637bb1664e36c0b2f05e368811be16a7fd10c93
Mohammad Abdullah <mabdul2202@gmail.com>
2020-10-10 16:24:33 +0100

    Added text and buttons

--- content.js
+++ content.js
@@ -1,12 +1,31 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action" ) {
-        var firstHref = $("a[href^='http']").eq(0).attr("href");
-  
-        console.log(firstHref);
 
-        // This line is new!
-        chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-      }
-    }
-  );
+
+var aup_content = '<div id="aup_content">';
+aup_content += '<div id="aup_header">Acceptable User Policy</div>';
+aup_content += '<div><div id="aup_text">';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += 'hgjhghb<br>hgj<br>sdfsfjh<br>gsdasdg';
+aup_content += '</div></div>';
+aup_content += '<div id="aup_buttons">';
+aup_content += '<div><button>I Agree</button></div>';
+aup_content += '<div><button>I Do Not Agree</button></div>';
+aup_content += '</div>';
+aup_content += '</div>';
+
+// If aup has been agreed do not display
+
+// If no record of agreed variable display aup
+$(document.body).append('<div id="aup_container">' + aup_content + '</div>');
+
+
